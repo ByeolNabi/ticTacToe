9b7bf6c99b6dc5abdfe654657926ff663e3515d8
ByeolNabi <narwhal_blast@naver.com>
2021-04-08 18:00:52 +0900

    commit all file

---
+++ ticTacToe1.py
@@ -0,0 +1,136 @@
+#ticTacToeV2.py
+
+#TTT 설명
+print("TicTacToe게임을 소개합니다.")
+print("3x3칸에 1줄을 먼저 만드는 사람이 승리합니다.")
+print("아래의 표는 돌의 위치를 숫자로 표현 한 것 입니다.")
+print("---------")
+print("|", 7, 8, 9, "|")
+print("|", 4, 5, 6, "|")
+print("|", 1, 2, 3, "|")
+print("---------")
+
+#TTT반복 트리거 0:반복 1:끝
+endGame = False 
+#전적 기록 생성(list)
+matchHistory = []
+
+#TTT시작
+while endGame == False:
+    print("게임을 시작합니다.")
+    #승자 선별
+    winner = False
+    #돌의 개수
+    numOfStone = 0
+    #돌 생성
+    stone = 'o'
+    #TTT판 생성
+    board = ['-'] * 10
+    #현재 상황
+    print("---------")
+    print("|", board[7], board[8], board[9], "|")
+    print("|", board[4], board[5], board[6], "|")
+    print("|", board[1], board[2], board[3], "|")
+    print("---------")
+    
+    #돌 나두기(게임 끝날 때까지)
+    while winner == False:
+        #   돌 나두기 안내
+        print("원하는 위치에 돌을 나둬주세요")
+        #   돌 위치 입력 데이터 선별
+        while True:
+            inData = input()
+            #   입력 데이터 타입 선별
+            while not len(inData) == 1 or not "1" <= inData <= "9":
+                print("한 자릿수 숫자를 입력해주세요")
+                inData = input()
+            #   정수로 변환
+            LoS = int(inData)
+            #   중복 배치 판단
+            if board[LoS] == '-':
+                break
+            else:
+                print("빈 자리에 배치해주세요")
+        #   돌 배치
+        board[LoS] = stone
+        #   현재상황
+            ##def## stoneChart()
+        print("\n---------")
+        print("|", board[7], board[8], board[9], "|")
+        print("|", board[4], board[5], board[6], "|")
+        print("|", board[1], board[2], board[3], "|")
+        print("---------")
+    
+        #승리 조건 검사 / 승리시 게임 끝
+        for i in [1, 4, 7]:
+            if board[i] == stone and board[i+1] == stone and board[i+2] == stone:
+                winner = True #승자 등장!
+                
+        for i in [1, 2, 3]:
+            if board[i] == stone and board[i+3] == stone and board[i+6] == stone:
+                winner = True
+    
+        if board[1] == stone and board[5] == stone and board[9] == stone:
+                winner = True
+    
+        if board[7] == stone and board[5] == stone and board[3] == stone:
+                winner = True
+    
+        ##def## 승자 등장 후 결과 저장 및 출력
+        if winner == True:
+            print("%s님의 승리." %stone)
+            matchHistory.append(stone)
+            oWin = matchHistory.count('o')
+            xWin = matchHistory.count('x')
+            draw = matchHistory.count('-')
+            print("o:%s승, x:%s승, 무승부:%s회" % (oWin, xWin, draw))
+    
+        #돌의 개수 세기
+        numOfStone += 1
+        
+        #9개의 돌을 다 놨을 때
+        if numOfStone == 9 and winner == False:
+            ##def## 무승부 저장 후 결과 출력
+            print("비겼습니다.")
+            matchHistory.append('-')
+            oWin = matchHistory.count('o')
+            xWin = matchHistory.count('x')
+            draw = matchHistory.count('-')
+            print("o:%s승, x:%s승, 무승부:%s회"  % (oWin, xWin, draw))
+            winner = True #승자가 있는건 아니지만 일단 게임은 끝내야지...
+            break
+    
+    ##def## reGame?
+        while winner == True:
+            print("한 번 더 하시겠습니까?(y/n)")
+            reGame = input()
+            if reGame == 'y' or reGame == 'Y':
+                break
+            elif reGame == 'n' or reGame == 'N':
+                print("플레이 해주셔서 감사합니다.")
+                endGame = True
+                break
+            else:
+                print("\ny 또는 n을 입력해주세요.")
+    
+        #돌 변경(x=o, o=x)
+        if stone == 'o':
+            stone = 'x'
+        else:
+            stone = 'o'
+
+
+
+
+'''
+vs 컴퓨터 만들기 
+'''
+
+'''
+승리 조건 검사하는 도중에 승리가 확정되면 축하 멘트를 넣어야했다.
+그러면 가로로 승리 세로로 승리를 검토하는 명령마다 축하 멘트를 넣어야해서 완전히 똑같은 축하멘트를 4번이나 복사 붙어넣기를 해야하는 상황이 되었다.
+축하 멘트를 함수로 만들어서 4번 넣는 글자수를 줄여 보기 좋게라고 만들까 라고 생각했다. 하지만 함수는 아직 익숙치 않았다. 매개변수와 인수를 관리하는 것을 못해서 최대한 함수 없이 만들어 보자고 생각했다.
+축하멘트를 반복문 밖으로 빼버리고 승리했다는 것을 알려주는 변수를 지정하고 그 변수가 True일때 축하멘트를 넣는 조건문은 만들어서 밖에 빼두었다.
+그렇게 해서 쓸때없는 반복멘트를 없앨 수 있었다.
+~안의 ~안의 함수 같은 끝도없이 중첩되려는 함수형태에 써먹으면 좋을 것 같다는 생각을 했다.
+'''
---
+++ ticTacToeFail.py
@@ -0,0 +1,133 @@
+#ticTacToe.py
+
+#변수 관리가 너무 어려워...
+
+#def 입력한 데이터가 올바른지 확인
+def LoSJudge():
+    global LoS
+    while True:
+        inData = input("돌을 놓을 숫자를 입력하세요\n")    
+        
+        if len(inData) == 1 and '1' <= inData <= '9':  
+            LoS = int(inData)
+            if board[LoS] != '-':
+                print("돌이 없는 곳을 선택해 주세요")
+            else:
+                break
+            #돌 위치 Location of Stone
+        else:
+            print("\n1~9까지의 숫자를 입력해주세요")
+                #입력된 데이터 선별 반복
+        board[LoS] = stone
+            
+#def 승리 조건을 달성한 플레이어가 있는지 확인하기
+def winnerJudge(stone):
+    for i in [1, 4, 7]:
+        if board[i] == stone and board[i+1] == stone and board[i+2] == stone:
+            print("%s님의 승리." %stone)
+            return True
+            
+    for i in [1, 2, 3]:
+        if board[i] == stone and board[i+3] == stone and board[i+6] == stone:
+            print("%s님의 승리." %stone)
+            return True
+        
+    if board[1] == stone and board[5] == stone and board[9] == stone:
+        print("%s님의 승리." %stone)
+        return True
+
+    if board[7] == stone and board[5] == stone and board[3] == stone:
+        print("%s님의 승리." %stone)
+        return True
+
+#def 틱택토 한 판
+def ticTacToe():
+
+    #ttt 틀
+    board = ['-'] * 10
+
+    print("---------")
+    print("|", board[7], board[8], board[9], "|")
+    print("|", board[4], board[5], board[6], "|")
+    print("|", board[1], board[2], board[3], "|")
+    print("---------") 
+
+
+
+    #돌 나두기 그후 돌 종류 변경 + 반복
+    stone = 'o'
+    numOfTurn = 0
+    win = False
+
+    while numOfTurn < 9 and win == False:
+        
+        LoSJudge()##돌 놓기 입력, 위치 판단
+        
+        numOfTurn = numOfTurn + 1
+        
+        print("%d번째 돌" % numOfTurn)
+        
+        print("---------")
+        print("|", board[7], board[8], board[9], "|")
+        print("|", board[4], board[5], board[6], "|")
+        print("|", board[1], board[2], board[3], "|")
+        print("---------\n")#현재상황판
+        
+        win = winnerJudge(stone)
+    while win == True:
+        print("한 번 더 하시겠습니까?(y/n)")
+        reGame = input()
+        if reGame == "y":
+            win = False
+            ticTacToe()
+            #틱택토 초기화
+        
+        elif reGame == "n":
+            win = False
+            #전적
+        
+        else:
+            print("y 또는 n을 입력해 주세요\n")
+
+            
+        ##밑에 이거를 어디에 넣어야 잘 들어간걸까
+    if stone == 'o':
+        stone = 'x'
+    else:
+        stone = 'o'
+            #다음에 두어야 할 돌 모양 변환 o면 x로, x면 o로
+
+#시작 설명
+print("TicTacToe게임을 소개합니다.")
+print("3x3칸에 1줄을 먼저 만드는 사람이 승리합니다.")
+print("아래의 표는 돌의 위치를 숫자로 표현 한 것 입니다.")
+print("---------")
+print("|", 7, 8, 9, "|")
+print("|", 4, 5, 6, "|")
+print("|", 1, 2, 3, "|")
+print("---------")
+
+ticTacToe()
+
+
+'''
+#게임 모드 선택
+print("\n게임 모드를 선택합니다.")
+print("[1] 1인모드 , [2] 2인모드")
+
+
+#선택에 따른 분기
+while True:
+    gameMode = input("\n원하는 게임 모드의 숫자를 입력해주세요")
+
+    if gameMode == "1":
+        print("1인 모드를 시작합니다")
+        #vs컴퓨터 프로그래밍
+        
+    elif gameMode == "2":
+        print("2인 모드를 시작합니다.")
+        #그냥 하면 됨
+
+    else:
+        print("숫자 1 또는 2를 입력해주세요")
+'''
